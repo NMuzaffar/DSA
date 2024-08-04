83f2b3e9d95591ec5c5566966ee87c0dffbb2ecb
Muzaffar Nurmukhammadov <mnurmukhammadov@liveperson.com>
2024-08-04 13:16:04 +0200

    Recursion, In-Place: https://leetcode.com/problems/reverse-string/

---
+++ src/LeetCode/easy/Problem344.java
@@ -0,0 +1,38 @@
+package LeetCode.easy;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * <a href="https://leetcode.com/problems/reverse-string/">Reverse String</a>
+ */
+public class Problem344 {
+
+  public static void main(String[] args) {
+    char[] s1 = "hello".toCharArray();
+    reverseString(s1);
+    assertArrayEquals("olleh".toCharArray(), s1);
+
+    char[] s2 = "Hannah".toCharArray();
+    reverseString(s2);
+    assertArrayEquals("hannaH".toCharArray(), s2);
+  }
+
+   /**
+    * Approach: Recursion, In-Place
+    * Time Complexity: O(n)
+    * Space Complexity: O(n) -> Recursion Stack
+    */
+   public static void reverseString(char[] s) {
+       helper(s, 0, s.length - 1);
+   }
+
+  private static void helper(char[] s, int left, int right) {
+    if (left >= right) {
+      return;
+    }
+    char temp = s[left];
+    s[left] = s[right];
+    s[right] = temp;
+    helper(s, left + 1, right - 1);
+  }
+}
