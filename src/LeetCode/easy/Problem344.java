5c9c9c1c26e061806fb8cfc0dcf24d519820229a
Muzaffar Nurmukhammadov <mnurmukhammadov@liveperson.com>
2024-08-04 13:17:02 +0200

    Two Pointers: https://leetcode.com/problems/reverse-string/

--- src/LeetCode/easy/Problem344.java
+++ src/LeetCode/easy/Problem344.java
@@ -17,22 +17,37 @@ public class Problem344 {
     assertArrayEquals("hannaH".toCharArray(), s2);
   }
 
-   /**
-    * Approach: Recursion, In-Place
-    * Time Complexity: O(n)
-    * Space Complexity: O(n) -> Recursion Stack
-    */
-   public static void reverseString(char[] s) {
-       helper(s, 0, s.length - 1);
-   }
-
-  private static void helper(char[] s, int left, int right) {
-    if (left >= right) {
-      return;
+  /**
+   * Approach: Two Pointers
+   * Time Complexity: O(n)
+   * Space Complexity: O(1)
+   */
+  public static void reverseString(char[] s) {
+    int left = 0;
+    int right = s.length - 1;
+    while (left < right) {
+      char temp = s[left];
+      s[left++] = s[right];
+      s[right--] = temp;
     }
-    char temp = s[left];
-    s[left] = s[right];
-    s[right] = temp;
-    helper(s, left + 1, right - 1);
   }
+
+//   /**
+//    * Approach: Recursion, In-Place
+//    * Time Complexity: O(n)
+//    * Space Complexity: O(n) -> Recursion Stack
+//    */
+//   public static void reverseString(char[] s) {
+//       helper(s, 0, s.length - 1);
+//   }
+//
+//  private static void helper(char[] s, int left, int right) {
+//    if (left >= right) {
+//      return;
+//    }
+//    char temp = s[left];
+//    s[left] = s[right];
+//    s[right] = temp;
+//    helper(s, left + 1, right - 1);
+//  }
 }
