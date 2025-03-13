808ca262b49f037fcf2dc32f42528bed0d96d01a
Muzaffar Nurmukhammadov <mnurmukhammadov@liveperson.com>
2025-03-13 22:17:22 +0100

    https://leetcode.com/problems/minimum-value-to-get-positive-step-by-step-sum/

--- src/LeetCode/easy/Problem1413.java
+++ src/LeetCode/easy/Problem1413.java
@@ -9,8 +9,8 @@ public class Problem1413 {
 
   public static void main(String[] args) {
     assertEquals(5, minStartValue(new int[] {-3,2,-3,4,2}));
-    assertEquals(1, minStartValue(new int[] {1, 2}));
-    assertEquals(5, minStartValue(new int[] {1, -2, -3}));
+    assertEquals(1, minStartValue(new int[] {1,2}));
+    assertEquals(5, minStartValue(new int[] {1,-2,-3}));
   }
 
   /**
@@ -18,58 +18,38 @@ public class Problem1413 {
    * Time Complexity: O(n) <br>
    * Space Complexity: O(1) <br>
    */
-  public static int minStartValue1(int[] nums) {
-
-
-    int n = nums.length;
-
-    int[] prefix = new int[n];
-    prefix[0] = nums[0];
-    for (int i = 1; i < n; i++) {
-      prefix[i] = prefix[i - 1] + nums[i];
-    }
-
-    int startValue = 1;
-    while (true) {
-      boolean allStepsPositive = false;
-      for (int prefixNum : prefix) {
-        if (prefixNum + startValue < 1) {
-          allStepsPositive = false;
-          break;
-        }
-        allStepsPositive = true;
-      }
-      if (allStepsPositive) {
-        return startValue;
-      }
-      startValue++;
-    }
-  }
-
-  /**
-   * Approach: Brute Force <br>
-   * Time Complexity: O(n) <br>
-   * Space Complexity: O(1) <br>
-   */
   public static int minStartValue(int[] nums) {
-    int startValue = 1;
-    while (true) {
-      int total = startValue;
-      boolean isValid = true;
-
-      for (int num : nums) {
-        total += num;
-        if (total < 1) {
-          isValid = false;
-          break;
-        }
-      }
-
-      if (isValid) {
-        return startValue;
-      } else {
-        startValue++;
-      }
+    int minVal = 0; // minimum step-by-step total
+    int total = 0; // current step-by-step total
+    for (int num : nums) {
+      total += num;
+      minVal = Math.min(minVal, total);
     }
+    return 1 - minVal; // (minVal + startValue = 1) <=> (startValue = 1 - minVal)
   }
+
+//  /**
+//   * Approach: Brute Force <br>
+//   * Time Complexity: O(n^2 * m) <br>
+//   * Space Complexity: O(1) <br>
+//   */
+//  public static int minStartValue(int[] nums) {
+//    int startValue = 1;
+//    while (true) {
+//      int total = startValue;
+//      boolean isValid = true;
+//      for (int num : nums) {
+//        total += num;
+//        if (total < 1) {
+//          isValid = false;
+//          break;
+//        }
+//      }
+//      if (isValid) {
+//        return startValue;
+//      } else {
+//        startValue++;
+//      }
+//    }
+//  }
 }
