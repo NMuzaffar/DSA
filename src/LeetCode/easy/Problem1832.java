a8b78a817dfb9ebd37cd6899677072282be8c09f
Muzaffar Nurmukhammadov <mnurmukhammadov@liveperson.com>
2025-04-01 19:25:45 +0200

    https://leetcode.com/problems/check-if-the-sentence-is-pangram/

--- src/LeetCode/easy/Problem1832.java
+++ src/LeetCode/easy/Problem1832.java
@@ -11,6 +11,52 @@ import java.util.Set;
 public class Problem1832 {
 
   public static void main(String[] args) {
+    assertTrue(checkIfPangram("thequickbrownfoxjumpsoverthelazydog"));
+    assertFalse(checkIfPangram("leetcode"));
   }
 
+  /**
+   * Approach: Hash Set <br>
+   * Time Complexity: O(n) <br>
+   * Space Complexity: O(1) <br>
+   */
+  public static boolean checkIfPangram(String sentence) {
+    Set<Character> seen = new HashSet<>();
+    for (char currChar : sentence.toCharArray()) {
+      seen.add(currChar);
+    }
+    return seen.size() == 26;
+  }
+
+//  /**
+//   * Approach: Use Array as Counter <br>
+//   * Time Complexity: O(n) <br>
+//   * Space Complexity: O(1) <br>
+//   */
+//  public static boolean checkIfPangram(String sentence) {
+//    boolean[] seen = new boolean[26];
+//    for (char currChar : sentence.toCharArray()) {
+//      seen[currChar - 'a'] = true;
+//    }
+//    for (boolean status : seen) {
+//      if (!status)
+//        return false;
+//    }
+//    return true;
+//  }
+//
+//  /**
+//   * Approach: Find letters one by one <br>
+//   * Time Complexity: O(n) <br>
+//   * Space Complexity: O(1) <br>
+//   */
+//  public static boolean checkIfPangram(String sentence) {
+//    for (int i = 0; i < 26; i++) {
+//      char currChar = (char) ('a' + i);
+//      if (sentence.indexOf(currChar) == -1) {
+//        return false;
+//      }
+//    }
+//    return true;
+//  }
 }
