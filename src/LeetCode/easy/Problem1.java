db95008b6519ead9fe5d76bc29c31168ab6a4881
Muzaffar Nurmukhammadov <mnurmukhammadov@liveperson.com>
2025-04-09 17:25:09 +0200

    https://leetcode.com/problems/two-sum/

--- src/LeetCode/easy/Problem1.java
+++ src/LeetCode/easy/Problem1.java
@@ -11,9 +11,9 @@ import java.util.Map;
 public class Problem1 {
 
   public static void main(String[] args) {
-    assertArrayEquals(new int[]{0,1}, twoSum(new int[]{2,7,11,15}, 9));
-    assertArrayEquals(new int[]{1,2}, twoSum(new int[]{3,2,4}, 6));
-    assertArrayEquals(new int[]{0,1}, twoSum(new int[]{3,3}, 6));
+    assertArrayEquals(new int[] {0,1}, twoSum(new int[] {2,7,11,15}, 9));
+    assertArrayEquals(new int[] {1,2}, twoSum(new int[] {3,2,4}, 6));
+    assertArrayEquals(new int[] {0,1}, twoSum(new int[] {3,3}, 6));
   }
 
   /**
