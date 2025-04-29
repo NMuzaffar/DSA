cf40ff6f755fabeedaa3acb2ae18fb208cea97ec
Muzaffar Nurmukhammadov <mnurmukhammadov@liveperson.com>
2025-04-29 13:58:43 +0200

    https://leetcode.com/problems/reverse-words-in-a-string-iii/

--- src/LeetCode/easy/Problem557.java
+++ src/LeetCode/easy/Problem557.java
@@ -35,27 +35,27 @@ public class Problem557 {
     return new String(chArray);
   }
 
-  /**
-   * Approach: Traverse and Reverse each character one by one <br>
-   * Time Complexity: O(n) <br>
-   * Space Complexity: O(n) <br>
-   */
-  public static String reverseWords1(String s) {
-    StringBuilder sb = new StringBuilder();
-    int lastSpaceIndex = -1;
-    for (int i = 0; i < s.length(); i++) {
-      if ((i == s.length() - 1) || Character.isWhitespace(s.charAt(i))) {
-        int reverseIndex = (i == s.length() - 1) ? i : (i - 1);
-        for (; reverseIndex > lastSpaceIndex; reverseIndex--) {
-          sb.append(s.charAt(reverseIndex));
-        }
-        if (i != s.length() - 1) {
-          sb.append(' ');
-        }
-        lastSpaceIndex = i;
-      }
-    }
-    return sb.toString();
-  }
+//  /**
+//   * Approach: Traverse and Reverse each character one by one <br>
+//   * Time Complexity: O(n) <br>
+//   * Space Complexity: O(n) <br>
+//   */
+//  public static String reverseWords(String s) {
+//    StringBuilder sb = new StringBuilder();
+//    int lastSpaceIndex = -1;
+//    for (int i = 0; i < s.length(); i++) {
+//      if ((i == s.length() - 1) || Character.isWhitespace(s.charAt(i))) {
+//        int reverseIndex = (i == s.length() - 1) ? i : (i - 1);
+//        for (; reverseIndex > lastSpaceIndex; reverseIndex--) {
+//          sb.append(s.charAt(reverseIndex));
+//        }
+//        if (i != s.length() - 1) {
+//          sb.append(' ');
+//        }
+//        lastSpaceIndex = i;
+//      }
+//    }
+//    return sb.toString();
+//  }
 }
 
