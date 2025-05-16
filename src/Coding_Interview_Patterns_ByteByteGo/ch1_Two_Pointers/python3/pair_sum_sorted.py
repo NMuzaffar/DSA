304ccecf0fd197be3604e22966f9b36218d9609e
Muzaffar Nurmukhammadov <nurmukhammadovm@gmail.com>
2025-05-16 22:00:09 +0200

    ByteByteGo: https://bytebytego.com/exercises/coding-patterns/two-pointers/pair-sum-sorted

--- src/Coding_Interview_Patterns_ByteByteGo/ch1_Two_Pointers/python3/pair_sum_sorted.py
+++ src/Coding_Interview_Patterns_ByteByteGo/ch1_Two_Pointers/python3/pair_sum_sorted.py
@@ -2,20 +2,6 @@ from typing import List
 
 
 '''
-Approach: Brute Force
-Time Complexity: O(n^2)
-Space Complexity: O(1)
-'''
-def pair_sum_sorted_brute_force(nums: List[int], target: int) -> List[int]:
-    n = len(nums)
-    for i in range(n):
-        for j in range(i + 1, n):
-            if nums[i] + nums[j] == target:
-                return [i, j]
-    return []
-
-
-'''
 Approach: Two Pointers
 Time Complexity: O(n)
 Space Complexity: O(1)
@@ -31,4 +17,18 @@ def pair_sum_sorted(nums: List[int], target: int) -> List[int]:
             right -= 1
         else:
             return [left, right]
+    return []
+
+
+'''
+Approach: Brute Force
+Time Complexity: O(n^2)
+Space Complexity: O(1)
+'''
+def pair_sum_sorted_brute_force(nums: List[int], target: int) -> List[int]:
+    n = len(nums)
+    for i in range(n):
+        for j in range(i + 1, n):
+            if nums[i] + nums[j] == target:
+                return [i, j]
     return []
