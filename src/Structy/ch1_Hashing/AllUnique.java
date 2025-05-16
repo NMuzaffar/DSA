7a2094fa0e633240b64887d9e884c2d70af94926
Muzaffar Nurmukhammadov <nurmukhammadovm@gmail.com>
2025-05-16 23:48:33 +0200

    Structy: https://structy.net/problems/premium/all-unique

---
+++ src/Structy/ch1_Hashing/AllUnique.java
@@ -0,0 +1,28 @@
+package Structy.ch1_Hashing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.*;
+
+/**
+ * <a href="https://structy.net/problems/premium/all-unique">AllUnique</a>
+ */
+public class AllUnique {
+
+  public static void main(String[] args) {
+    assertTrue(allUnique(List.of("q", "r", "s", "a")));
+    assertFalse(allUnique(List.of("q", "r", "s", "a", "r", "z")));
+    assertTrue(allUnique(List.of("red", "blue", "yellow", "green", "orange")));
+    assertFalse(allUnique(List.of("cat", "cat", "dog")));
+    assertFalse(allUnique(List.of("a", "u", "t", "u", "m", "n")));
+  }
+
+  /**
+   * Time Complexity: O(n) <br>
+   * Space Complexity: O(n) <br>
+   */
+  public static boolean allUnique(List<String> items) {
+    Set<String> uniqueItems = new HashSet<>(items);
+    return uniqueItems.size() == items.size();
+  }
+}
