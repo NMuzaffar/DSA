b215a0794d25895b40e7d24d0fdf3a179f9aa264
Muzaffar Nurmukhammadov <mnurmukhammadov@liveperson.com>
2025-05-31 17:56:56 +0200

    ByteByteGo: https://bytebytego.com/exercises/coding-patterns/two-pointers/largest-container

--- src/Coding_Interview_Patterns_ByteByteGo/ch1_two_pointers/python3/largest_container.py
+++ src/Coding_Interview_Patterns_ByteByteGo/ch1_two_pointers/python3/largest_container.py
@@ -2,10 +2,37 @@ from typing import List
 
 
 '''
-Approach: 
-Time Complexity: O()
-Space Complexity: O()
+Approach: Two Pointers (Inward Traversal)
+Time Complexity: O(n)
+Space Complexity: O(1)
 '''
 def largest_container(heights: List[int]) -> int:
-    pass
+    max_water = 0
+    left = 0
+    right = len(heights) - 1
+    while left < right:
+        water = min(heights[left], heights[right]) * (right - left)
+        max_water = max(max_water, water)
+        if heights[left] < heights[right]:
+            left += 1
+        elif heights[left] > heights[right]:
+            right -= 1
+        else:
+            left += 1
+            right -= 1
+    return max_water
 
+
+# '''
+# Approach: Brute Force
+# Time Complexity: O(n^2)
+# Space Complexity: O(1)
+# '''
+# def largest_container(heights: List[int]) -> int:
+#     max_water = 0
+#     n = len(heights)
+#     for i in range(n):
+#         for j in range(i + 1, n):
+#             water = min(heights[i], heights[j]) * (j - i)
+#             max_water = max(max_water, water)
+#     return max_water
