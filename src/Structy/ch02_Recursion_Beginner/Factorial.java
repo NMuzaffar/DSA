61fd46eda14c6d35bc10518682919f018b29196c
Muzaffar Nurmukhammadov <mnurmukhammadov@liveperson.com>
2025-06-18 09:43:07 +0200

    Structy: https://structy.net/problems/premium/factorial

---
+++ src/Structy/ch02_Recursion_Beginner/Factorial.java
@@ -0,0 +1,29 @@
+package Structy.ch02_Recursion_Beginner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * <a href="https://structy.net/problems/premium/factorial">Factorial</a>
+ */
+public class Factorial {
+
+  public static void main(String[] args) {
+    assertEquals(6L, factorial(3));
+    assertEquals(720L, factorial(6));
+    assertEquals(6402373705728000L, factorial(18));
+    assertEquals(1L, factorial(1));
+    assertEquals(6227020800L, factorial(13));
+    assertEquals(1L, factorial(0));
+  }
+
+  /**
+   * Time Complexity: O(n) <br>
+   * Space Complexity: O(n) <br>
+   */
+  public static long factorial(int n) {
+    if (n == 0) {
+      return 1;
+    }
+    return n * factorial(n - 1);
+  }
+}
