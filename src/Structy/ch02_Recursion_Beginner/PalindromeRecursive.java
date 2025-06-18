89db05be1be85a9d41baad831dcc77674093fb1e
Muzaffar Nurmukhammadov <mnurmukhammadov@liveperson.com>
2025-06-18 19:18:30 +0200

    Structy: https://structy.net/problems/premium/palindrome-recursive

---
+++ src/Structy/ch02_Recursion_Beginner/PalindromeRecursive.java
@@ -0,0 +1,33 @@
+package Structy.ch02_Recursion_Beginner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * <a href="https://structy.net/problems/premium/palindrome-recursive">Palindrome Recursive</a>
+ */
+public class PalindromeRecursive {
+
+  public static void main(String[] args) {
+    assertTrue(palindrome("pop"));
+    assertTrue(palindrome("kayak"));
+    assertFalse(palindrome("pops"));
+    assertFalse(palindrome("boot"));
+    assertTrue(palindrome("rotator"));
+    assertFalse(palindrome("abcbca"));
+    assertTrue(palindrome(""));
+  }
+
+  /**
+   * Time Complexity: O(n^2) <br>
+   * Space Complexity: O(n^2) <br>
+   */
+  public static boolean palindrome(String s) {
+    if (s.isEmpty() || s.length() == 1) {
+      return true;
+    }
+    if (s.charAt(0) != s.charAt(s.length() - 1)) {
+      return false;
+    }
+    return palindrome(s.substring(1, s.length() - 1));
+  }
+}
