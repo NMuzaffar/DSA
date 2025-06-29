93e4fde8c68e7beb316cab492a4a892626bcbf9f
Muzaffar Nurmukhammadov <mnurmukhammadov@liveperson.com>
2025-06-29 16:47:24 +0200

    LeetCode: https://leetcode.com/problems/make-the-string-great/

--- src/LeetCode/easy/Problem1544.java
+++ src/LeetCode/easy/Problem1544.java
@@ -17,38 +17,38 @@ public class Problem1544 {
   }
 
   /**
-   * Approach: Stack <br>
+   * Approach: Two Pointers <br>
    * Time Complexity: O(n) <br>
    * Space Complexity: O(n) <br>
    */
   public static String makeGood(String s) {
-    StringBuilder stack = new StringBuilder();
-    for (char c : s.toCharArray()) {
-      if (!stack.isEmpty() && Math.abs(stack.charAt(stack.length() - 1) - c) == 32) {
-        stack.deleteCharAt(stack.length() - 1);
+    StringBuilder sb = new StringBuilder(s);
+    int end = 0;
+    for (int curr = 0; curr < sb.length(); curr++) {
+      if (end > 0 && Math.abs(sb.charAt(curr) - sb.charAt(end - 1)) == 32) {
+        end--;
       } else {
-        stack.append(c);
+        sb.setCharAt(end, sb.charAt(curr));
+        end++;
       }
     }
-    return stack.toString();
+    return sb.substring(0, end);
   }
 
 //  /**
-//   * Approach: Two Pointers <br>
+//   * Approach: Stack (StringBuilder) <br>
 //   * Time Complexity: O(n) <br>
 //   * Space Complexity: O(n) <br>
 //   */
 //  public static String makeGood(String s) {
-//    StringBuilder sb = new StringBuilder(s);
-//    int end = 0;
-//    for (int curr = 0; curr < sb.length(); curr++) {
-//      if (end > 0 && Math.abs(sb.charAt(curr) - sb.charAt(end - 1)) == 32)
-//        end--;
-//      else {
-//        sb.setCharAt(end, sb.charAt(curr));
-//        end++;
+//    StringBuilder stack = new StringBuilder();
+//    for (char c : s.toCharArray()) {
+//      if (!stack.isEmpty() && Math.abs(stack.charAt(stack.length() - 1) - c) == 32) {
+//        stack.deleteCharAt(stack.length() - 1);
+//      } else {
+//        stack.append(c);
 //      }
 //    }
-//    return sb.substring(0, end);
+//    return stack.toString();
 //  }
 }
