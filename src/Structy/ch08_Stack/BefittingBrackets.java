41a4bba46bcb11071d54349d1bc7934343cd51d1
Muzaffar Nurmukhammadov <mnurmukhammadov@liveperson.com>
2025-07-05 09:17:15 +0200

    Structy: https://structy.net/problems/premium/befitting-brackets

---
+++ src/Structy/ch08_Stack/BefittingBrackets.java
@@ -0,0 +1,47 @@
+package Structy.ch08_Stack;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Map;
+
+/**
+ * <a href="https://structy.net/problems/premium/befitting-brackets">Befitting Brackets</a>
+ */
+public class BefittingBrackets {
+
+  public static void main(String[] args) {
+    assertTrue(befittingBrackets("(){}[](())"));
+    assertTrue(befittingBrackets("({[]})"));
+    assertFalse(befittingBrackets("[][}"));
+    assertFalse(befittingBrackets("{[]}({}"));
+    assertFalse(befittingBrackets("[]{}(}[]"));
+    assertTrue(befittingBrackets("[]{}()[]"));
+    assertFalse(befittingBrackets("]{}"));
+    assertTrue(befittingBrackets(""));
+    assertFalse(befittingBrackets("{[(}])"));
+  }
+
+  /**
+   * Time Complexity: O(n) <br>
+   * Space Complexity: O(n) <br>
+   */
+  public static boolean befittingBrackets(String str) {
+    Map<Character, Character> bracketsMapping = Map.of(
+        '(', ')',
+        '{', '}',
+        '[', ']');
+    Deque<Character> stack = new ArrayDeque<>();
+    for (char c : str.toCharArray()) {
+      if (bracketsMapping.containsKey(c)) {
+        stack.push(c);
+      } else {
+        if (stack.isEmpty() || c != bracketsMapping.get(stack.pop())) {
+          return false;
+        }
+      }
+    }
+    return stack.isEmpty();
+  }
+}
