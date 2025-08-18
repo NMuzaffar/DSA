fb3ec75b4b366801f94d1442d6250cd407b38c3f
Muzaffar Nurmukhammadov <mnurmukhammadov@liveperson.com>
2025-08-18 21:46:09 +0200

    Structy: https://structy.net/problems/island-count

---
+++ src/Structy/ch06_Graph_I/IslandCount.java
@@ -0,0 +1,88 @@
+package Structy.ch06_Graph_I;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.*;
+
+/**
+ * <a href="https://structy.net/problems/island-count">Island Count</a>
+ */
+public class IslandCount {
+
+  public static void main(String[] args) {
+    assertEquals(3, islandCount(List.of(
+        List.of("W", "L", "W", "W", "W"),
+        List.of("W", "L", "W", "W", "W"),
+        List.of("W", "W", "W", "L", "W"),
+        List.of("W", "W", "L", "L", "W"),
+        List.of("L", "W", "W", "L", "L"),
+        List.of("L", "L", "W", "W", "W")
+    )));
+
+    assertEquals(4, islandCount(List.of(
+        List.of("L", "W", "W", "L", "W"),
+        List.of("L", "W", "W", "L", "L"),
+        List.of("W", "L", "W", "L", "W"),
+        List.of("W", "W", "W", "W", "W"),
+        List.of("W", "W", "L", "L", "L")
+    )));
+
+    assertEquals(1, islandCount(List.of(
+        List.of("L", "L", "L"),
+        List.of("L", "L", "L"),
+        List.of("L", "L", "L")
+    )));
+
+    assertEquals(0, islandCount(List.of(
+        List.of("W", "W"),
+        List.of("W", "W"),
+        List.of("W", "W")
+    )));
+  }
+
+  /**
+   * Approach: Graph DFS (Recursive) <br>
+   * Time Complexity: O(m * n) <br>
+   * Space Complexity: O(m * n) <br>
+   */
+  public static int islandCount(List<List<String>> grid) {
+    int count = 0;
+    int rowLen = grid.size();
+    int colLen = grid.get(0).size();
+    boolean[][] visited = new boolean[rowLen][colLen];
+    for (int row = 0; row < rowLen; row++) {
+      for (int col = 0; col < colLen; col++) {
+        if (!visited[row][col] && grid.get(row).get(col).equals("L")) {
+          count++;
+          visited[row][col] = true;
+          dfs(row, col, grid, visited);
+        }
+      }
+    }
+    return count;
+  }
+
+  private static final int[][] DIRECTIONS = new int[][] {
+      {1, 0},
+      {-1, 0},
+      {0, 1},
+      {0, -1}
+  };
+
+  private static void dfs(int row, int col, List<List<String>> grid, boolean[][] visited) {
+    for (int[] direction : DIRECTIONS) {
+      int nextRow = row + direction[0];
+      int nextCol = col + direction[1];
+      if (isValidPos(grid, nextRow, nextCol) && !visited[nextRow][nextCol]) {
+        visited[nextRow][nextCol] = true;
+        dfs(nextRow, nextCol, grid, visited);
+      }
+    }
+  }
+
+  private static boolean isValidPos(List<List<String>> grid, int row, int col) {
+    return 0 <= row && row < grid.size()
+        && 0 <= col && col < grid.get(0).size()
+        && grid.get(row).get(col).equals("L");
+  }
+}
