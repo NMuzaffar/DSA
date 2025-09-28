ea3d3972930fa90d37159ceb615947eca77779a5
Muzaffar Nurmukhammadov <nurmukhammadovm@gmail.com>
2025-09-28 19:37:34 +0200

    LeetCode: https://leetcode.com/problems/destroying-asteroids/

---
+++ src/LeetCode/medium/Problem2126.java
@@ -0,0 +1,33 @@
+package LeetCode.medium;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.*;
+
+/**
+ * <a href="https://leetcode.com/problems/destroying-asteroids/">2126. Destroying Asteroids</a>
+ */
+public class Problem2126 {
+
+  public static void main(String[] args) {
+      assertTrue(asteroidsDestroyed(10, new int[] {3,9,19,5,21}));
+      assertTrue(asteroidsDestroyed(5, new int[] {4,9,23,4}));
+  }
+
+  /**
+   * Approach: Greedy <br>
+   * Time Complexity: O(n * log(n)) <br>
+   * Space Complexity: O(log(n)) <br>
+   */
+  public static boolean asteroidsDestroyed(int mass, int[] asteroids) {
+    long currMass = mass;
+    Arrays.sort(asteroids);
+    for (int asteroid : asteroids) {
+      if (asteroid > currMass) {
+        return false;
+      }
+      currMass += asteroid;
+    }
+    return true;
+  }
+}
