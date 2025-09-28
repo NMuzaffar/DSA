ac0499f496e2436aa02c603c6116be7846d60eb4
Muzaffar Nurmukhammadov <nurmukhammadovm@gmail.com>
2025-09-28 21:06:18 +0200

    Structy: https://structy.net/problems/fib

--- src/Structy/ch07_Dynamic_Programming/Fib.java
+++ src/Structy/ch07_Dynamic_Programming/Fib.java
@@ -7,18 +7,18 @@ import java.util.Map;
 
 /**
  * <a href="https://structy.net/problems/fib">Fib</a>
- */
+ * */
 public class Fib {
 
   public static void main(String[] args) {
-      assertEquals(0, fib(0));
-      assertEquals(1, fib(1));
-      assertEquals(1, fib(2));
-      assertEquals(2, fib(3));
-      assertEquals(3, fib(4));
-      assertEquals(5, fib(5));
-      assertEquals(9227465, fib(35));
-      assertEquals(1836311903, fib(46));
+    assertEquals(0, fib(0));
+    assertEquals(1, fib(1));
+    assertEquals(1, fib(2));
+    assertEquals(2, fib(3));
+    assertEquals(3, fib(4));
+    assertEquals(5, fib(5));
+    assertEquals(9227465, fib(35));
+    assertEquals(1836311903, fib(46));
   }
 
   /**
@@ -31,10 +31,10 @@ public class Fib {
 
   private static int fibMemoized(int n, Map<Integer, Integer> memo) {
     if (n == 0 || n == 1) {
-        return n;
+      return n;
     }
     if (memo.containsKey(n)) {
-        return memo.get(n);
+      return memo.get(n);
     }
     int result = fibMemoized(n - 1, memo) + fibMemoized(n - 2, memo);
     memo.put(n, result);
